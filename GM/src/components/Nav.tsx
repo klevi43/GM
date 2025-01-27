a5b37af697229a7d87ed3f54c6f590a1f590558b
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-01-27 15:14:45 +0900

    Changed links to NavLink

--- GM/src/components/Nav.tsx
+++ GM/src/components/Nav.tsx
@@ -1,35 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import Lines from '../assets/svgs/lines.svg'
-import { Link } from 'react-router';
-import Home from '../pages/Home';
+import Lines from "../assets/svgs/lines.svg";
+import { NavLink } from "react-router";
+import Home from "../pages/Home";
 
 interface Props {
   isNavMenuOpen: boolean;
   isLoggedIn: boolean;
   toggleNavBar: () => void;
 }
-const Nav = ({isNavMenuOpen, toggleNavBar, isLoggedIn}: Props) => {
+const Nav = ({ isNavMenuOpen, toggleNavBar, isLoggedIn }: Props) => {
   return (
-  <> 
-    <div className='nav__container'>
+    <>
+      <div className="nav__container">
+        <nav className="nav">
+          <NavLink className="logo" to="/">
+            GM<span className="logo-desktop">Growth Mindset</span>
+          </NavLink>
+          <button onClick={toggleNavBar} className="nav__toggle">
+            <img src={Lines} alt="" />
+          </button>
 
-    
-      <nav className='nav'>
-        <Link className='logo' to='/'>GM<span className='logo-desktop'>Growth Mindset</span></Link>
-        <button onClick={toggleNavBar} className='nav__toggle'><img src={Lines} alt="" /></button>
-    
-      <ul className={`list nav__list ${isNavMenuOpen ? "collapsible--expanded" : "collapsible--hidden"}`}>
-          {!isLoggedIn && <li className='nav__item'><Link className='link text' to='/login'>Login</Link>
-          </li>}
-          
-          {isLoggedIn && <li className='nav__item'><Link className='link text' to='/logout'>Logout</Link></li>}
-          <li className='nav__item'><Link className='link text' to="/workouts">My Workouts</Link><div className='underline'></div></li>  
-        </ul> 
-      </nav>  
-    </div>
-  </>
-    )
-}
+          <ul
+            className={`list nav__list ${
+              isNavMenuOpen ? "collapsible--expanded" : "collapsible--hidden"
+            }`}
+          >
+            {!isLoggedIn && (
+              <li className="nav__item">
+                <NavLink className="link text" to="/login">
+                  Login
+                </NavLink>
+              </li>
+            )}
 
-export default Nav
+            {isLoggedIn && (
+              <li className="nav__item">
+                <NavLink className="link text" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            )}
+            <li className="nav__item">
+              <NavLink className="link text" to="/workouts">
+                My Workouts
+              </NavLink>
+              <div className="underline"></div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
---
+++ GM/src/pages/.d.ts
@@ -0,0 +1,6 @@
+import Home from "./Home";
+
+export * from "./Home";
+export * from "./Login";
+export * from "./MyExercises";
+export * from "./MyWorkouts";
