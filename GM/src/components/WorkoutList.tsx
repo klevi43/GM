91d781e006b3e637f2ef120b042fce2cb689d0fe
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-01-30 16:34:53 +0900

    Added error messages and loading messages to workout pages

--- GM/src/components/WorkoutList.tsx
+++ GM/src/components/WorkoutList.tsx
@@ -4,20 +4,35 @@ import { Link } from "react-router";
 
 interface Props {
   workouts?: Workout[];
+  error: Error | null;
+  isLoading: boolean;
   handleDelete(id: number): void;
 }
-const WorkoutList = ({ workouts, handleDelete }: Props) => {
+const WorkoutList = ({ workouts, error, isLoading, handleDelete }: Props) => {
   return (
     <>
+      {!error && !isLoading && !workouts && (
+        <div>
+          <p className="text center">Nothing to show</p>
+        </div>
+      )}
+      {isLoading && (
+        <div className="outline workout__list-item">
+          <p className="text center ">Loading your workouts...</p>
+        </div>
+      )}
+      {error && (
+        <div className="outline workout__list-item">
+          <p className="text text-error center ">{error.message}</p>
+        </div>
+      )}
       <ul className="list">
-        {!workouts && <p className="text">Nothing to show</p>}
         {workouts &&
           workouts.map((workout) => (
             <li key={workout.id} className="outline workout__list-item">
               <Link className="link text" to={`/workouts/${workout.id}`}>
                 <div className="space-between">
                   <h2>{workout.name}</h2>
-
                   <button
                     onClick={() => handleDelete(workout.id)}
                     className="btn btn-primary text link"
--- GM/src/pages/MyWorkouts.tsx
+++ GM/src/pages/MyWorkouts.tsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 import { Link } from "react-router";
 import { useState } from "react";
 import Workout from "../models/Workout";
-import List from "../components/WorkoutList";
+import WorkoutList from "../components/WorkoutList";
 import axios from "axios";
 import useWorkouts from "../hooks/useWorkouts";
 import { UseWorkoutsContextType } from "../context/WorkoutsProvider";
@@ -24,10 +24,14 @@ const MyWorkouts = () => {
 
   const fetchWorkouts = () => {
     return axios
-      .get<Workout[]>("http://localhost:3500/workouts")
+      .get<Workout[]>("http://localhost:3500/worko?uts")
       .then((res) => res.data);
   };
-  const { data: workouts } = useQuery({
+  const {
+    data: workouts,
+    error,
+    isLoading,
+  } = useQuery<Workout[], Error>({
     // unique identifier, used for caching
     queryKey: ["id", "name", "date"],
     queryFn: fetchWorkouts,
@@ -44,8 +48,14 @@ const MyWorkouts = () => {
       <header className="nav__offset">
         <h1 className="heading">My Workouts</h1>
       </header>
+
       <div className="container workout__container">
-        <List workouts={workouts} handleDelete={handleDelete} />
+        <WorkoutList
+          workouts={workouts}
+          error={error}
+          isLoading={isLoading}
+          handleDelete={handleDelete}
+        />
       </div>
     </>
   );
