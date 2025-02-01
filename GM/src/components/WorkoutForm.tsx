7c20cf2b83971222f1ed0b539d0fbbc35b3dc510
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-02-01 13:46:48 +0900

    WORKOUT FORM submitting data nolonger triggers a rerender of the page will continue to search fro the cause of the issue

--- GM/src/components/WorkoutForm.tsx
+++ GM/src/components/WorkoutForm.tsx
@@ -4,51 +4,14 @@ import Workout from "../models/Workout";
 import axios from "axios";
 const WorkoutForm = () => {
   const ref = useRef<HTMLInputElement>(null);
-  const queryClient = useQueryClient();
-  //           data received from backed, error, data sent to backend
-  const addWorkout = useMutation<Workout, Error, Workout>({
-    mutationFn: (workout: Workout) => {
-      return axios
-        .post<Workout>("http://localhost:3500/workouts", workout)
-        .then((res) => res.data);
-    },
-
-    onSuccess: (savedWorkout, newWorkout) =>
-      // APPROACHES: INVALIDATE CACHE, update data in cache directly
-      queryClient.setQueryData<Workout[]>(["workouts"], (workouts) => [
-        savedWorkout,
-        ...(workouts || []),
-      ]),
-  });
-  if (ref.current) ref.current.value = "";
   return (
     <>
-      {addWorkout.error && (
-        <div className="outline workout__list-item">
-          <p className="text text-error center">{addWorkout.error.message}</p>
-        </div>
-      )}
-      <form
-        action=""
-        onSubmit={(event) => {
-          // prevents form from being sent to server
-          event.preventDefault();
-          if (ref.current && ref.current.value) {
-            addWorkout.mutate({
-              id: 4,
-              name: ref.current?.value,
-              date: new Date(),
-            });
-          }
-        }}
-      >
+      <form action="">
         <div>
           <input ref={ref} type="text" />
         </div>
         <div>
-          <button disabled={addWorkout.isLoading} className="btn btn--primary">
-            {addWorkout.isLoading ? " Adding..." : "Add"}
-          </button>
+          <button className="btn btn--primary">Add</button>
         </div>
       </form>
     </>
