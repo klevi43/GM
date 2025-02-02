0923744bbd9e7796016748ec98bd10404ecb85f0
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-02-02 19:04:30 +0900

    MYEXERCISES Starteed designing the exercises page

--- GM/src/pages/MyExercises.tsx
+++ GM/src/pages/MyExercises.tsx
@@ -5,6 +5,32 @@ const MyExercises = () => {
   return (
     <>
       <Nav />
+      <header className="nav__offset">
+        <h1 className="heading">WORKOUT TITLE</h1>
+      </header>
+      <div className="container outline">
+        <div className="">
+          <h2 className="subheading">EXERCISE TITLE</h2>
+        </div>
+        <div className="center">
+          <table className="outline table">
+            <tr className="space-between">
+              <th>Set</th>
+              <th>Weight</th>
+              <th>Reps</th>
+            </tr>
+            <tr className="space-between">
+              <td>1</td>
+              <td>15</td>
+              <td>12</td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <button className="btn btn--primary">Add</button>
+          <button className="btn btn--primary">Delete</button>
+        </div>
+      </div>
     </>
   );
 };
